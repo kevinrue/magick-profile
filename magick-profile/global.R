9703733fa7dc0d3d6ae8ea7776c27cf827a01e34
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-04-13 11:55:36 +0100

    black if k-mean is 1

--- magick-profile/global.R
+++ magick-profile/global.R
@@ -90,7 +90,9 @@ make_jitterplot <- function(matrix,
         xy_coord <- xy_coord[keep,]
     }
     # Identify clusters
-    xy_coord$kmean <- as.factor(kmeans(x = as.matrix(xy_coord[, c("row", "col")]), centers = k_centers)$cluster)
+    if (k_centers > 1) {
+        xy_coord$cluster <- as.factor(kmeans(x = as.matrix(xy_coord[, c("row", "col")]), centers = k_centers)$cluster)
+    }
     # Make coordinates look centered
     x_center <- x_range[2] / 2
     y_center <- -y_range[2] / 2
@@ -99,16 +101,26 @@ make_jitterplot <- function(matrix,
     x_range <- x_range - x_center
     y_range <- y_range + y_center # we work in negative values here
     # Plot
-    ggplot(xy_coord) +
-        geom_jitter(aes(row, col, color=kmean),
-                    size = point_size,
-                    width = jitter,
-                    height = jitter) +
+    gg <- ggplot(xy_coord)
+    if (k_centers > 1) {
+        gg <- gg + geom_jitter(aes(row, col, color=cluster),
+                               size = point_size,
+                               width = jitter,
+                               height = jitter)
+    }  else {
+        gg <- gg + geom_jitter(aes(row, col),
+                               color="black",
+                               size = point_size,
+                               width = jitter,
+                               height = jitter)
+    }
+    gg <- gg +
         coord_cartesian(xlim = x_range, ylim = y_range) +
         labs(x = paste(axis_label_prefix, 1L),
              y = paste(axis_label_prefix, 2L)) +
         guides(color="none") +
         theme(panel.background = element_blank())
+    gg
 }
 
 make_original <- function(object) {
